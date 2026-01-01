4788a9d692d71d82a4c57b68e34059474b54a18a
cacheninetynine <88933951+cacheninetynine@users.noreply.github.com>
2026-01-01 22:14:59 +0300

    prs are REALLY welcome

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,7 +2,7 @@
 
 BOARD_VENDOR := xiaomi
 
-DEVICE_PATH := device/xiaomi/missi # says missi but this is specifically for serenity
+DEVICE_PATH := device/xiaomi/serenity
 
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
@@ -16,18 +16,20 @@ TARGET_2ND_CPU_VARIANT := cortex-a55
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 
-BOARD_SUPER_PARTITION_SIZE := 5368709120
 TARGET_BOARD_PLATFORM := ums9230
 
 TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz
 TARGET_PREBUILT_RAMDISK := $(DEVICE_PATH)/prebuilt/ramdisk.cpio
+BOARD_USES_VENDOR_RAMDISK := true
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_KERNEL_IMAGE_NAME := Image.gz
 
-BOARD_RAMDISK_OFFSET := 0x01000000
-BOARD_KERNEL_CMDLINE := console=no_console_suspend keep_bootcon irqaffinity=thread irqs=noirq debug irqfixup irqpoll rcu_nocbs=rcu_nocb_poll cma swiotlb coherent_pool profile=highres=ntp_tick_adj clocksource=clock nohz_skew_tick no_smp nr_cpus maxcpus cgroup_disable=cgroup_debug cgroup_no_v1 audit=audit_backlog_limit softlockup_panic nowatchdog nosoftlockup watchdog_thresh ftrace ftrace_dump_on_oops traceoff_on_warning alloc_snapshot trace_options=trace_clock tp_printk tp_printk_stop_on_boot trace_buf_size=tracing_thresh trace_event=kprobe_event mminit_loglevel=BJ
-BOARD_KERNEL_BASE := 0x80080000
+BOARD_BOOTCONFIG += androidboot.hardware=serenity
+BOARD_BOOTCONFIG += androidboot.dtbo_idx=0
+
+BOARD_KERNEL_CMDLINE := console=ttyS1,115200n8 bootconfig bootconfig
 BOARD_KERNEL_PAGESIZE := 4096
-BOARD_BOOTIMG_HEADER_VERSION := 2
+BOARD_BOOTIMG_HEADER_VERSION := 4
 
 BOARD_USES_RECOVERY_AS_BOOT := true
---
+++ device.mk
@@ -0,0 +1,6 @@
+PRODUCT_NAME := serenity
+PRODUCT_DEVICE := serenity
+PRODUCT_NAME := xiaomi
+PRODUCT_MODEL := "serenity"
+
+PRODUCT_AOSP_AB := true
